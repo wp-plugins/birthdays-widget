32eeec97bba6b96d3ef398810b8ff24dbf6550d7
lion2486 <lion2486@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-28 14:57:05 +0000

    v1.1
    git-svn-id: https://plugins.svn.wordpress.org/birthdays-widget/tags/1.1@811914 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-birthdays-widget-installer.php
@@ -0,0 +1,48 @@
+<?php
+	class Birthdays_Widget_Installer{
+		
+		static function install() {
+			global $wpdb;
+			
+			//create the new table
+			$table_name = $wpdb->prefix . "birthdays"; 
+			
+			$sql = "CREATE TABLE $table_name (
+					  id int(11) NOT NULL AUTO_INCREMENT,
+					  name text  NOT NULL,
+					  date date NOT NULL,
+					  PRIMARY KEY  id(id)
+					) DEFAULT CHARSET=utf8;";
+
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			
+			dbDelta( $sql );
+			
+			add_option( 'Birthdays_Widget_Installed', '1' );
+			
+			return;
+		}
+		
+		static function uninstall() {
+			global $wpdb;
+			
+			$table_name = $wpdb->prefix . "birthdays";
+			
+			$sql = "DROP TABLE IF EXISTS $table_name;";
+			
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			
+			dbDelta( $sql );
+			
+			delete_option( 'Birthdays_Widget_Installed' );
+		}
+		
+		static function activate() {
+			if( ! current_user_can ( 'activate_plugins' ) )
+				return "You cannot activate it";
+			if( ! get_option ( 'Birthdays_Widget_Installed' ) )
+				return Birthdays_Widget_Installer::install();
+			
+			return;
+		}
+	}
